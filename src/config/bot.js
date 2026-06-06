61f98a4ac0dcf17d8871de9ec864cedb018bc720
Aaron Li <51304175+aaron8004963@users.noreply.github.com>
2026-06-06 11:52:12 -0700

    Update bot activity name in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "with denji 🤍",
+        name: "🚬 Watching over Denji and Power",
         // Activity type number (0 = Playing).
         type: 4, 
       },
